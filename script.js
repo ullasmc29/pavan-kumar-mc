c6c85583df03a1a24f2680e09332e0c8c1e80657
ullasmc29 <83940712+ullasmc29@users.noreply.github.com>
2023-11-26 15:28:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let data = "<b>The alarm is set four times a day</b>"+
+                "<ul>"+
+                    "<li>Morning - BreakFast</li>"+
+                    "<li>Afternoon - Lunch</li>"+
+                    "<li>Evening - Snaks</li>"+
+                    "<li>Night - Dinner</li>"+
+                "</ul>"
+
+function alarmSet(){
+    document.getElementById('testt').innerHTML = data
+}
